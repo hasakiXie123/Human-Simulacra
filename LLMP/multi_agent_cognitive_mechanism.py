a669e86ed8c59d22cd5f003cf9b6c76c8d50b944
hasakiXie123 <76777406+hasakiXie123@users.noreply.github.com>
2024-08-17 10:14:38 +0800

    Update multi_agent_cognitive_mechanism.py

--- LLMP/multi_agent_cognitive_mechanism.py
+++ LLMP/multi_agent_cognitive_mechanism.py
@@ -60,7 +60,7 @@ class Memory_agent:
         summary = self.sum(messages).content
         return summary
 
-    def Index_construction(self, index):
+    def Save_index_file(self, index):
         # Construct the index file (index.json) with the structure: {"num1": "Memory_Summary1", "num2": "Memory_Summary2"....}
         file_path = os.path.join(self.path, "index.json")
         if not os.path.exists(file_path):  # 检查文件是否存在
@@ -73,8 +73,8 @@ class Memory_agent:
             with open(file_path, 'w') as file:
                 json.dump(index, file)
 
-    def Add_long_memory(self, memory):
-        # Add long-term memory to long_memory.json
+    def Save_long_memory(self, memory):
+        # Save long-term memory to long_memory.json
         file_path = os.path.join(self.path, "long_memory.json")  # 连接文件夹路径和文件名
         if not os.path.exists(self.path):  # 检查文件是否存在
             with open(file_path, 'w') as file:
@@ -84,21 +84,21 @@ class Memory_agent:
         else:
             print(f"File {file_path} already exists, replace it with a new one!")
             with open(file_path, 'w') as file:
-                json.dump(memory, file)
-
-    def Add_short_memory(self, memory):
-        # Add short-term memory to short_memory.txt
-        file_path = os.path.join(self.path, "short_memory.txt")  # 连接文件夹路径和文件名
-        if not os.path.exists(self.path):  # 检查文件是否存在
-            with open(file_path, 'w') as file:
-                file.write(memory)  # 写入内容
-                file.write('\n')
-            print(f"File {file_path} created successfully!")
-        else:
-            print(f"File {file_path} already exists")
-            with open(file_path, 'a') as file:
-                file.write(memory)
-                file.write('\n')
+                json.dump(memory, file)     
+    
+    # def Add_short_memory(self, memory):
+    #     # Add short-term memory to short_memory.txt
+    #     file_path = os.path.join(self.path, "short_memory.txt")  # 连接文件夹路径和文件名
+    #     if not os.path.exists(self.path):  # 检查文件是否存在
+    #         with open(file_path, 'w') as file:
+    #             file.write(memory)  # 写入内容
+    #             file.write('\n')
+    #         print(f"File {file_path} created successfully!")
+    #     else:
+    #         print(f"File {file_path} already exists")
+    #         with open(file_path, 'a') as file:
+    #             file.write(memory)
+    #             file.write('\n')
 
     def Search(self, result_list):
         # Retrieve relevant memories from long_memory.json based on result_list
@@ -323,10 +323,18 @@ class Top_agent:
             if introduction["Name"] == self.name:
                 flag = True
                 self.infos = introduction['Basic_infos']
+                if introduction["Extra"]:
+                    # Convert the Extra dictionary to a string format
+                    extra_info_str = ', '.join([f"{key}: {value}" for key, value in introduction["Extra"].items()])
+                    character_infos = introduction['Basic_infos'].strip('"\n') 
+                    # Append the Extra information to the Basic_infos string and restore the original format
+                    character_infos = f"\"\"\n" + character_infos.rstrip(".") + f", {extra_info_str}.\n\"\"\n"
+                    self.infos = character_infos
                 self.personality_traits = introduction['Personality_traits']
                 self.biography = introduction['Content']
+                break
         if flag == False:
-            print("Can not find the information.")
+            print(Fore.RED + "Can not find the information." + Style.RESET_ALL)
             sys.exit(1)
 
         # Initialize three agents
@@ -343,12 +351,60 @@ class Top_agent:
         
         if not os.path.exists(os.path.join(Memory_Directory, self.name)):
             os.mkdir(os.path.join(Memory_Directory, self.name))
-            print("------ Starting to constructing the long memory for " + self.name)
+            print(Fore.RED + "Long memory not found. Constructing long memory..." + Style.RESET_ALL)
             self.long_memory_construction()
-            print("------ Finishing to constructing the long memory for " + self.name)
+            print(Fore.RED + "Long memory construction completed." + Style.RESET_ALL)
         
-        
+    def add_new_attributes(self, new_attributes):
+        ## adding new character attributes to the character's profile
+        if not isinstance(new_attributes, dict):
+            raise ValueError("new attributes must be a dictionary")
+        print(Fore.RED + "addng new attributes..." + Style.RESET_ALL)
+        with open(Introductions_Path, "r", encoding="UTF-8") as file:
+            introductions = json.load(file)
+            for idx, introduction in enumerate(introductions):
+                if introduction["Name"] == self.name:
+                    flag = True
+                    Extra = introduction["Extra"]
+                    Extra.update(new_attributes)
+                    introductions[idx]["Extra"] = Extra
+                    break
+        if flag == False:
+            print(Fore.RED + "Can not find the information." + Style.RESET_ALL)
+            sys.exit(1)
+        with open(Introductions_Path, "w", encoding="UTF-8") as file:
+            json.dump(introductions, file, ensure_ascii=False, indent=4, separators=(',', ': ')) 
+        print(Fore.RED + "Task completed." + Style.RESET_ALL) 
 
+    def add_long_memory(self, new_life_story):
+        if not isinstance(new_life_story, str):
+            raise ValueError("new life story must be a string")
+        
+        print(Fore.RED + "Constructing long memory..." + Style.RESET_ALL)
+        
+        index_file_path = os.path.join(Memory_Directory, self.name, "index.json")
+        with open(index_file_path, 'r') as file:
+            index = json.load(file)
+        memory_file_path = os.path.join(Memory_Directory, self.name, "long_memory.json")
+        with open(memory_file_path, 'r') as file:
+            long_memory = json.load(file)
+            
+        memory_content = self.Thinking_Agent.Memory_construction(new_life_story)
+        thinking = self.Thinking_Agent.Thinking_Memory_construction(memory_content)
+        emotion = self.Emotion_Agent.Memory_construction(new_life_story)
+        memory_summary = self.Memory_Agent.Summary(memory_content, emotion)
+        memory = {"Memory Content": memory_content, "Thinking": thinking, "Emotion": emotion}
+        long_memory_chunk = {"Memory_Summary" : memory_summary , "Memory" : memory}
+        keys = [int(k) for k in index.keys()]
+        new_key = str(max(keys) + 1).zfill(3)
+        index[new_key] = memory_summary
+        long_memory[new_key] = long_memory_chunk
+        
+        self.Memory_Agent.Save_long_memory(long_memory)
+        self.Memory_Agent.Save_index_file(index)
+        
+        print(Fore.RED + "Long memory construction completed. You can start the conversation." + Style.RESET_ALL)
+    
     def long_memory_construction(self):
         # Complete process function for constructing long-term memory
         
@@ -381,14 +437,14 @@ class Top_agent:
                 long_memory_chunk = {"Memory_Summary" : memory_summary , "Memory" : memory}
                 index[string_num] = memory_summary
                 long_memory[string_num] = long_memory_chunk
-                print(string_num, end="\r")  # Display progress
-                print(index)
-                print("--------------------------")
-                print(long_memory)
-                print("--------------------------")
+                print(Fore.RED + string_num + Style.RESET_ALL, end="\r")  # Display progress
+                # print(index)
+                # print("--------------------------")
+                # print(long_memory)
+                # print("--------------------------")
                 temp_chunk = file.readline()
-        self.Memory_Agent.Add_long_memory(long_memory)
-        self.Memory_Agent.Index_construction(index)
+        self.Memory_Agent.Save_long_memory(long_memory)
+        self.Memory_Agent.Save_index_file(index)
         
     def multi_turn_chat(self):
         # Multi-turn conversation, input "exit" to terminate conversation
@@ -515,6 +571,124 @@ class Top_agent:
             )
         return user_prompt
 
+def Bandwagon_chat_with_naive_prompt(character_name, query, chat_history=None):
+    chat = ChatOpenAI(
+        openai_api_key = API_KEY,
+        openai_api_base = BASE_URL,
+        model = Model_for_evaluation
+    )
+    flag = False
+    with open(Introductions_Path, "r", encoding="UTF-8") as file:
+        introductions = json.load(file)
+    for introduction in introductions:
+        if introduction["Name"] == character_name:
+            flag = True
+            character_infos = introduction['Basic_infos']
+            if introduction["Extra"]:
+                # Convert the Extra dictionary to a string format
+                extra_info_str = ', '.join([f"{key}: {value}" for key, value in introduction["Extra"].items()])
+                character_infos = character_infos.strip('"\n') 
+                # Append the Extra information to the Basic_infos string and restore the original format
+                character_infos = f"\"\"\n" + character_infos.rstrip(".") + f", {extra_info_str}.\n\"\"\n"
+            personality_traits = introduction['Personality_traits']
+            character_biography = introduction['Content']
+            break
+    if flag == False:
+        print(Fore.RED + "Can not find the information." + Style.RESET_ALL)
+        sys.exit(1)
+        
+    System_prompt = Naive_simulacra_prompt_template.format(
+            character_name = character_name,
+            basic_information = character_infos,
+            personality_traits = personality_traits,
+            introduction = character_biography,
+        )
+    messages = [
+        SystemMessage(content=System_prompt)
+    ]
+    if chat_history:
+        messages += chat_history
+    messages.append(HumanMessage(content=query))
+    agents_ans = chat(messages)
+    return agents_ans.content
+
+def Bandwagon_chat_with_blank_model(query, chat_history=None):
+    chat = ChatOpenAI(
+        openai_api_key = API_KEY,
+        openai_api_base = BASE_URL,
+        model = Model_for_evaluation
+    )
+    messages = []
+    if chat_history:
+        messages += chat_history
+    messages.append(HumanMessage(content=query))
+    agents_ans = chat(messages)
+    return agents_ans.content
+
+def Bandwagon_chat_with_naive_rag(character_name, query, chat_history=None):
+    chat = ChatOpenAI(
+        openai_api_key = API_KEY,
+        openai_api_base = BASE_URL,
+        model = Model_for_evaluation
+    )
+    flag = False
+    with open(Introductions_Path, "r", encoding="UTF-8") as file:
+        introductions = json.load(file)
+    for introduction in introductions:
+        if introduction["Name"] == character_name:
+            flag = True
+            character_infos = introduction['Basic_infos']
+            if introduction["Extra"]:
+                # Convert the Extra dictionary to a string format
+                extra_info_str = ', '.join([f"{key}: {value}" for key, value in introduction["Extra"].items()])
+                character_infos = character_infos.strip('"\n') 
+                # Append the Extra information to the Basic_infos string and restore the original format
+                character_infos = f"\"\"\n" + character_infos.rstrip(".") + f", {extra_info_str}.\n\"\"\n"
+            personality_traits = introduction['Personality_traits']
+            character_biography = introduction['Content']
+            break
+    if flag == False:
+        print(Fore.RED + "Can not find the information." + Style.RESET_ALL)
+        sys.exit(1)
+    from langchain.document_loaders import PyPDFLoader
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    from langchain.embeddings.openai import OpenAIEmbeddings
+    from langchain.vectorstores import Chroma
+        
+    loader = PyPDFLoader(os.path.join(Story_Directory, character_name, character_name + ".pdf"))
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=512,
+        chunk_overlap=32,
+    )
+    docs = text_splitter.split_documents(pages)
+    embed_model = OpenAIEmbeddings(
+        openai_api_base = OPENAI_BASE_URL, openai_api_key = OPENAI_API_KEY
+    )
+        
+    vectorstore = Chroma.from_documents(documents=docs, embedding=embed_model, collection_name="openai_embed")
+        
+    System_prompt = Naive_simulacra_prompt_template.format(
+            character_name = character_name,
+            basic_information = character_infos,
+            personality_traits = personality_traits,
+            introduction = character_biography,
+        )
+    messages = [
+        SystemMessage(content=System_prompt)
+    ]
+    if chat_history:
+        messages += chat_history
+    search_results = vectorstore.similarity_search(query, k=3)
+    source_knowledge = "\n".join([x.page_content for x in search_results])
+    rag_prompt = Naive_rag_simulacra_prompt_template.format(
+            source_knowledge = source_knowledge
+        )
+    messages.append(SystemMessage(content=rag_prompt))
+    messages.append(HumanMessage(content=query))
+    agents_ans = chat(messages)
+    return agents_ans.content
+    
 
 def Multi_turn_chat_with_naive_prompt(character_name):
     chat = ChatOpenAI(
@@ -529,10 +703,17 @@ def Multi_turn_chat_with_naive_prompt(character_name):
         if introduction["Name"] == character_name:
             flag = True
             character_infos = introduction['Basic_infos']
+            if introduction["Extra"]:
+                # Convert the Extra dictionary to a string format
+                extra_info_str = ', '.join([f"{key}: {value}" for key, value in introduction["Extra"].items()])
+                character_infos = character_infos.strip('"\n') 
+                # Append the Extra information to the Basic_infos string and restore the original format
+                character_infos = f"\"\"\n" + character_infos.rstrip(".") + f", {extra_info_str}.\n\"\"\n"
             personality_traits = introduction['Personality_traits']
             character_biography = introduction['Content']
+            break
     if flag == False:
-        print("Can not find the information.")
+        print(Fore.RED + "Can not find the information." + Style.RESET_ALL)
         sys.exit(1)
         
     System_prompt = Naive_simulacra_prompt_template.format(
@@ -563,7 +744,34 @@ def Multi_turn_chat_with_naive_prompt(character_name):
         chat_history.append(agents_ans)
             
     print(Fore.RED + "The conversation is over." + Style.RESET_ALL)
-    
+
+def Multi_turn_chat_with_blank_model():
+    # blank model, which does not know anything about the character.
+    chat = ChatOpenAI(
+        openai_api_key = API_KEY,
+        openai_api_base = BASE_URL,
+        model = Model_for_evaluation
+    )
+    current_messages = []
+    chat_history = []
+    while True:
+        query = input()
+        if query.lower() == "exit":
+            break     
+        if chat_history:
+            history = "\n".join(chat_history)
+            context = "You're chatting with someone in a coffee shop. This is your conversation record: <<<\n" + history + ">>>"
+            current_messages.append(SystemMessage(content=context))
+        else:
+            current_messages.append(SystemMessage(content="You're chatting with someone in a coffee shop."))
+
+        current_messages.append(HumanMessage(content=query))
+        agents_ans = chat(current_messages).content
+        print(Fore.GREEN + agents_ans + Style.RESET_ALL)
+        chat_history.append(query)
+        chat_history.append(agents_ans)
+            
+    print(Fore.RED + "The conversation is over." + Style.RESET_ALL)
 
 def Multi_turn_chat_with_naive_rag(character_name):
     chat = ChatOpenAI(
@@ -578,10 +786,17 @@ def Multi_turn_chat_with_naive_rag(character_name):
         if introduction["Name"] == character_name:
             flag = True
             character_infos = introduction['Basic_infos']
+            if introduction["Extra"]:
+                # Convert the Extra dictionary to a string format
+                extra_info_str = ', '.join([f"{key}: {value}" for key, value in introduction["Extra"].items()])
+                character_infos = character_infos.strip('"\n') 
+                # Append the Extra information to the Basic_infos string and restore the original format
+                character_infos = f"\"\"\n" + character_infos.rstrip(".") + f", {extra_info_str}.\n\"\"\n"
             personality_traits = introduction['Personality_traits']
             character_biography = introduction['Content']
+            break
     if flag == False:
-        print("Can not find the information.")
+        print(Fore.RED + "Can not find the information." + Style.RESET_ALL)
         sys.exit(1)
     from langchain.document_loaders import PyPDFLoader
     from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -645,7 +860,7 @@ def main():
 
     # Add arguments
     parser.add_argument("--character_name", type=str, required=True, help="Name of the character")
-    parser.add_argument("--method", type=str, choices=["prompt", "rag", "macm"], required=True, help="Method of conversation: prompt, rag, or macm")
+    parser.add_argument("--method", type=str, choices=["prompt", "rag", "macm", "none"], required=True, help="Method of conversation: prompt, rag, none or macm")
     parser.add_argument("--temperature", type=float, default=0.0, help="Temperature for the model")
 
     # Parse arguments
@@ -663,15 +878,18 @@ def main():
     elif args.method == "macm":
         print(Fore.RED + f"Starting multi-turn chat with MACM method for {args.character_name}. Type 'exit' to end the chat." + Style.RESET_ALL)
         agent = Top_agent(args.character_name, temperature=args.temperature)
-        memory_path = os.path.join(Memory_Directory, args.character_name, "long_memory.json")
-        if not os.path.exists(memory_path):
-            print("Long memory not found. Constructing long memory...")
-            agent.long_memory_construction()
-            print("Long memory construction completed.")
         agent.multi_turn_chat()
-    else:
+    elif args.method == "prompt":
         print(Fore.RED + f"Starting multi-turn chat with prompt method for {args.character_name}. Type 'exit' to end the chat." + Style.RESET_ALL)
         Multi_turn_chat_with_naive_prompt(args.character_name)
+    else:
+        print(Fore.RED + f"Starting multi-turn chat with blank model, which does not know anything about the character. Type 'exit' to end the chat." + Style.RESET_ALL)
+        Multi_turn_chat_with_blank_model(args.character_name)
+        
+    # agent = Top_agent("Mary Jones")
+    # agent.add_new_attributes({"Favorite Color": "Green", "Favorite Author": "Ada Lovelace"})
+    # agent.add_long_memory("Mary's favorite color is green, and her favorite author is Ada Lovelace.")
+    
 
 if __name__ == "__main__":
     main()
