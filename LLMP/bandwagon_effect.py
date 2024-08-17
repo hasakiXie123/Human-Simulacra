1a1899cec4c0d7f891377aa2204642587f4ba3b8
hasakiXie123 <76777406+hasakiXie123@users.noreply.github.com>
2024-08-17 10:10:49 +0800

    Update bandwagon_effect.py

--- LLMP/bandwagon_effect.py
+++ LLMP/bandwagon_effect.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import os
 from colorama import Fore, Style
 from Config.config import *
 from langchain.schema import (
@@ -8,7 +9,7 @@ from langchain.schema import (
     AIMessage
 )
 from prompt_templates import Bandwagon_effect_system_prompt_template, Bandwagon_effect_controlled_system_prompt_template, Bandwagon_effect_user_prompt_template, Bandwagon_effect_controlled_user_prompt_template
-from multi_agent_cognitive_mechanism import Top_agent
+from multi_agent_cognitive_mechanism import Top_agent, Bandwagon_chat_with_naive_prompt, Bandwagon_chat_with_naive_rag, Bandwagon_chat_with_blank_model
 
 ### [standard line, comparison line 1, comparison line 2, comparison line 3, correct response, group response]
 experiment_config = [
@@ -36,21 +37,24 @@ def colorful_print(chat_history):
     print(Fore.RED + f"Below is the experiment log" + Style.RESET_ALL)
     for message in chat_history:
         if isinstance(message, AIMessage):
+            print("-------------------------------")
             print(Fore.GREEN + message.content + Style.RESET_ALL)
         elif isinstance(message, HumanMessage):
-            print(message.content)
+            # print(message.content)
+            pass
         else:
             ## system
-            print(Fore.RED + message.content + Style.RESET_ALL)
+            # print(Fore.RED + message.content + Style.RESET_ALL)
+            pass
             
 
-def MACM_bandwagon_effect(character_name, temperature = 0.0, control=True):
+def Bandwagon_effect(character_name, method="macm", temperature = 0.0, control=True):
     """
-    Simulates the bandwagon effect using multi-agent cognitive mechanisms.
+    Simulates the bandwagon effect.
     """
-    print(Fore.RED + f"Starting Simulates the bandwagon effect using multi-agent cognitive mechanisms." + Style.RESET_ALL)
+    print(Fore.RED + f"Starting Simulates the bandwagon effect using " + method + " mechanisms." + Style.RESET_ALL)
     print(Fore.RED + f"Current character: " + character_name + Style.RESET_ALL)
-    critical_subject = Top_agent(character_name, temperature=temperature)
+    
     System_prompt = Bandwagon_effect_system_prompt_template.format(
         standard_len = experiment_config[0][0],
         len_1 = experiment_config[0][1],
@@ -65,7 +69,21 @@ def MACM_bandwagon_effect(character_name, temperature = 0.0, control=True):
         len_2 = experiment_config[0][2],
         len_3 = experiment_config[0][3]
     )
-    answer = critical_subject.bandwagon_chat(query=System_prompt)
+    if method == "macm":
+        critical_subject = Top_agent(character_name, temperature=temperature)
+        memory_path = os.path.join(Memory_Directory, character_name, "long_memory.json")
+        if not os.path.exists(memory_path):
+            print("Long memory not found. Constructing long memory...")
+            critical_subject.long_memory_construction()
+            print("Long memory construction completed.")
+        
+        answer = critical_subject.bandwagon_chat(query=System_prompt)
+    elif method == "prompt":
+        answer = Bandwagon_chat_with_naive_prompt(character_name, System_prompt)
+    elif method == "rag":
+        answer = Bandwagon_chat_with_naive_rag(character_name, System_prompt)
+    else:
+        answer = Bandwagon_chat_with_naive_prompt(character_name, System_prompt)
     chat_history = [
         HumanMessage(content=System_prompt),
         AIMessage(content=answer)
@@ -90,22 +108,38 @@ def MACM_bandwagon_effect(character_name, temperature = 0.0, control=True):
                 len_3 = experiment_config[i][3],
                 group_response = experiment_config[i][5]
             )
-        answer = critical_subject.bandwagon_chat(query=User_prompt, chat_history=chat_history)
+        if method == "macm":
+            answer = critical_subject.bandwagon_chat(query=User_prompt, chat_history=chat_history)
+        elif method == "prompt":
+            answer = Bandwagon_chat_with_naive_prompt(character_name, User_prompt, chat_history)
+        else:
+            answer = Bandwagon_chat_with_blank_model(User_prompt, chat_history)
         chat_history.append(HumanMessage(content=User_prompt))
         chat_history.append(AIMessage(content=answer))
     colorful_print(chat_history)
         
+
 def main():
-    parser = argparse.ArgumentParser(description="Simulate the bandwagon effect using multi-agent cognitive mechanisms.")
-    parser.add_argument('--character_name', type=str, required=True, help='Name of the character')
+    # Initialize argument parser
+    parser = argparse.ArgumentParser(description="Simulate the bandwagon effect using base prompt/rag/multi-agent cognitive mechanisms.")
+
+    # Add arguments
+    parser.add_argument("--character_name", type=str, required=True, help="Name of the character")
+    parser.add_argument("--method", type=str, choices=["prompt", "rag", "macm", "none"], required=True, help="Method of conversation: prompt, rag, none or macm")
     parser.add_argument('--control', action='store_true', help='Use control group')
-    
+
+    # Parse arguments
     args = parser.parse_args()
+
     # Check if character_name is in Character_list
     if args.character_name not in Character_list:
         print(f"Error: {args.character_name} is not in the Character_list.")
         sys.exit(1)
-    MACM_bandwagon_effect(args.character_name, control=args.control)
+
+    # Execute based on method
+    Bandwagon_effect(args.character_name, args.method, control=args.control)
+
+
 
 if __name__ == "__main__":
     main()
